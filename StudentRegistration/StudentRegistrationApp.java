083665d16cd54d16ab56382ae51b75cd1ee75364
csaksith <saksitca@mail.uc.edu>
2025-04-25 22:29:45 -0400

    complete student registration

--- StudentRegistration/StudentRegistrationApp.java
+++ StudentRegistration/StudentRegistrationApp.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class StudentRegistrationApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Student Registration Form");
+		System.out.print("\nEnter first name: ");
+		String firstName = sc.nextLine();
+		System.out.print("Enter last name: ");
+		String lastName = sc.nextLine();
+		System.out.print("Enter year of birth: ");
+		int year = sc.nextInt();
+		System.out.println("\nWelcome "+firstName+" "+lastName+"!");
+		System.out.println("Your registration is complete.");
+		System.out.println("Your temporary password is: "+firstName+"*"+year);
 	}
 
 }
