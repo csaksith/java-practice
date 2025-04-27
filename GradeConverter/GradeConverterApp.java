35a1385667fd3a661751c37202acdaab90080023
csaksith <saksitca@mail.uc.edu>
2025-04-27 10:30:47 -0400

    complete grade converter

--- GradeConverter/GradeConverterApp.java
+++ GradeConverter/GradeConverterApp.java
@@ -1,9 +1,47 @@
+import java.util.Scanner;
 
 public class GradeConverterApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the Letter Grade Converter");
+		// user input for numerical grade (numGrade)
+		// for loop 
+			// if numGrade>=88 -> print "letter grade: A"
+		// continue? 
+		
+		String choice = "y";
+		while(choice.equalsIgnoreCase("y")) {
+			String message = "Letter grade: ";
+			System.out.print("\nEnter numerical grade: ");
+			int numGrade = sc.nextInt();
+			if (numGrade>=88) {
+				String letterGrade = "A";
+				System.out.println(message+letterGrade);
+			}
+			else if (numGrade>=80) {
+				String letterGrade="B";
+				System.out.println(message+letterGrade);
+			}
+			else if (numGrade>=67) {
+				String letterGrade="C";
+				System.out.println(message+letterGrade);
+			}
+			else if (numGrade>=60) {
+				String letterGrade="D";
+				System.out.println(message+letterGrade);
+			}
+			else {
+				String letterGrade="F";
+				System.out.println(message+letterGrade);
+			}
+			System.out.print("\nContinue? (y/n): ");
+			choice =sc.next();
+		}
+		System.out.println("goodbye!");
+	
+	
+	
 	}
 
 }
