9ee3c3b89e8c9f2646f0bfd79fa9fb218a74a347
csaksith <saksitca@mail.uc.edu>
2025-04-28 20:22:09 -0400

    complete travel time calculator

--- TravelTimeCalculator/TravelTimeCalcApp.java
+++ TravelTimeCalculator/TravelTimeCalcApp.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
 
 public class TravelTimeCalcApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello!");
-
-		System.out.println("Goodbye!");
+		System.out.println("Welcome to the Travel Time Calculator!!");
+		// while choice = "y"
+		// user input miles
+		// user input miles per hour
+		// convert travel time
+		// get hour in int by doing miles/mph
+		// get minutes by subtracting int and get double?
+		// convert minutes by multiply by 60
+		Scanner sc = new Scanner(System.in);
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.print("\nEnter miles: ");
+			double miles = sc.nextDouble();
+			System.out.print("Enter miles per hour: ");
+			double mph = sc.nextDouble();
+			double hours = miles / mph;
+			int intHours = (int) hours;
+			double min = hours - intHours;
+			double minutes = min * 60;
+			int wholeMin = (int) minutes;
+			System.out.println("\nEstimated travel time");
+			System.out.println("======================");
+			System.out.println("Hours: " + intHours);
+			System.out.println("Minutes: " + wholeMin);
+			System.out.print("\nContinue? (y/n): ");
+			choice = sc.next();
+		}
+		System.out.println("\nGoodbye!");
 	}
 
 }
