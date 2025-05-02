db20f5d16d12c4e722f8934a2c6641cc54a34c5e
csaksith <saksitca@mail.uc.edu>
2025-05-02 19:24:57 -0400

    complete factorial calculator

--- FactorialCalculator/FactorialCalculatorApp.java
+++ FactorialCalculator/FactorialCalculatorApp.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
 
 public class FactorialCalculatorApp {
 
 	public static void main(String[] args) {
-		System.out.println("hello!");
+		System.out.println("Welcome to the Factorial Calculator!");
+		Scanner sc = new Scanner(System.in);
+		String choice = "y";
+		// user input integer between 1-10
+		// output the factor of input integer
+		// continue?
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.print("Enter an integer that's greater than 0 and less than 10: ");
+			int integer = sc.nextInt();
+			int factorial = 1;
+			for (int i = 1; i <= integer; i++) {
+				factorial *= i;
+			}
+			System.out.println("The factorial of " + integer + " is " + factorial + ".");
+			System.out.print("Continue? (y/n): ");
+			choice = sc.next();
+		}
+
 		System.out.println("bye!");
 	}
 
