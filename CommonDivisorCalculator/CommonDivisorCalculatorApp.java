f931ceeb2622c5b8b5f7c3a502f3766599bd04e1
csaksith <saksitca@mail.uc.edu>
2025-05-04 15:50:48 -0400

    complete common divisor calculator

--- CommonDivisorCalculator/CommonDivisorCalculatorApp.java
+++ CommonDivisorCalculator/CommonDivisorCalculatorApp.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
 
 public class CommonDivisorCalculatorApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+		System.out.println("Greatest Common Divisor Finder!\n");
+
+		// user input first number and second number
+		// subtract second number from first number
+		// do until first number is less than second number
+		// then swap
+		// subtract second number from first number
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter first number: ");
+		int x = sc.nextInt();
+		System.out.print("Enter second number: ");
+		int y = sc.nextInt();
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			while (x != y) {
+				if (x > y) {
+					x = x - y;
+				} else {
+					y = y - x;
+				}
+			}
+			System.out.println("Greatest common divisor: " + y);
+			System.out.print("\nContinue? (y/n): ");
+			choice = sc.next();
+		}
 		System.out.println("Goodbye!");
 	}
 
