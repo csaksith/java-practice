46db10b73248178f3823b9ab2850734d0a161e0d
csaksith <saksitca@mail.uc.edu>
2025-05-12 10:30:23 -0400

    complete contact list

---
+++ ContactList/Contact.java
@@ -0,0 +1,47 @@
+
+public class Contact {
+	public String firstName;
+	public String lastName;
+	public String email;
+	public String phone;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String displayContact() {
+		return "--------------------------------------------\n" + "---- Current Contact -----------------------\n"
+				+ "--------------------------------------------\n" + "Name:         " + firstName + " " + lastName
+				+ "\nEmail Address: " + email + "\nPhone Number:  " + phone
+				+ "\n--------------------------------------------";
+	}
+
+}
--- ContactList/ContactListApp.java
+++ ContactList/ContactListApp.java
@@ -1,9 +1,49 @@
+import java.util.Scanner;
 
 public class ContactListApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the Contact List Application!");
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			Contact contact = new Contact();
+			String firstName = getString(sc, "Enter first name: ");
+			String lastName = getString(sc, "Enter last name: ");
+			String email = getString(sc, "Enter email: ");
+			String phone = getString(sc, "Enter phone: ");
+			contact.setFirstName(firstName);
+			contact.setLastName(lastName);
+			contact.setEmail(email);
+			contact.setPhone(phone);
+
+			System.out.println();
+			System.out.println(contact.displayContact());
+
+			choice = getYesOrNo(sc, "\nContinue? (y/n): ");
+
+		}
 		System.out.println("Goodbye!");
+		sc.close();
 	}
 
+	public static String getString(Scanner sc, String prompt) {
+		System.out.print(prompt);
+		return sc.nextLine();
+	}
+
+	public static String getYesOrNo(Scanner sc, String prompt) {
+		String input = "";
+		boolean isValid = false;
+		while (!isValid) {
+			System.out.print(prompt);
+			input = sc.nextLine().toLowerCase();
+			if (input.equals("y") || input.equals("n")) {
+				isValid = true;
+			} else {
+				System.out.println("Error! Please enter 'y' or 'n'.");
+			}
+		}
+		return input;
+	}
 }
