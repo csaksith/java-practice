4ff2209e7679584a5f63b05c5e3737bed23a3bd3
csaksith <saksitca@mail.uc.edu>
2025-05-31 13:54:56 -0400

    added HR boilerplate

--- HRArraysDS/Solution.java
+++ HRArraysDS/Solution.java
@@ -1,9 +1,45 @@
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class Solution {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+class Result {
+
+	/*
+	 * Complete the 'reverseArray' function below.
+	 *
+	 * The function is expected to return an INTEGER_ARRAY. The function accepts
+	 * INTEGER_ARRAY a as parameter.
+	 */
+
+	public static List<Integer> reverseArray(List<Integer> a) {
+		// Write your code here
 
 	}
 
+}
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+		List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+				.map(Integer::parseInt).collect(toList());
+
+		List<Integer> res = Result.reverseArray(arr);
+
+		bufferedWriter.write(res.stream().map(Object::toString).collect(joining(" ")) + "\n");
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
 }
