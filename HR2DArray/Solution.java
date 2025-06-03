af20c3ab1d1d6197f2fb5e5bca82efb50d3fd9a1
csaksith <saksitca@mail.uc.edu>
2025-06-02 22:42:18 -0400

    complete HR 2D arrays

--- HR2DArray/Solution.java
+++ HR2DArray/Solution.java
@@ -1,9 +1,43 @@
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+		List<List<Integer>> arr = new ArrayList<>();
+
+		IntStream.range(0, 6).forEach(i -> {
+			try {
+				arr.add(Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" ")).map(Integer::parseInt)
+						.collect(toList()));
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		});
+
+		bufferedReader.close();
+
+		int maxSum = -63;
+		for (int i = 0; i <= 3; i++) {
+			for (int j = 0; j <= 3; j++) {
+				int currentSum = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2)
+						+ arr.get(i + 1).get(j + 1) + arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1)
+						+ arr.get(i + 2).get(j + 2);
+				if (currentSum > maxSum) {
+					maxSum = currentSum;
+				}
+			}
+		}
+		System.out.println(maxSum);
 	}
 
 }
